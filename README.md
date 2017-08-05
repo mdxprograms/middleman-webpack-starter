b65b3f3001cc9b745190147f90bebbfdb6358b03
Josh Waller <mdx.programs@gmail.com>
2017-08-05 02:11:41 -0400

    start the engines

---
+++ README.md
@@ -0,0 +1,16 @@
+# Middleman Webpack Starter
+
+```sh
+$ git clone git@github.com:mdxprograms/middleman-webpack-starter.git
+
+$ middleman init -T git@github.com:mdxprograms/middleman-webpack-starter.git .
+
+$ bundle install && yarn install
+```
+
+Serve and build
+```sh
+$ bundle exec middleman s
+
+$ bundle exec middleman b
+```
---
+++ config.rb
@@ -0,0 +1,17 @@
+activate :external_pipeline,
+         name: :webpack,
+         command: build? ?  "npm run build" : "npm run start",
+         source: ".tmp/dist",
+         latency: 1
+
+configure :development do
+  activate :livereload
+end
+
+set :css_dir, 'assets/stylesheets'
+set :js_dir, 'assets/javascript'
+set :images_dir, 'images'
+
+configure :build do
+  activate :asset_hash, ignore: [/\.jpg\Z/, /\.png\Z/]
+end
